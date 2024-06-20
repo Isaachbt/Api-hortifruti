f1b7524a1088ec4ae4a02957070eb1d44dae79a2
Isaachbt <isaac.silva1478@gmail.com>
2024-06-20 01:51:30 +0100

    commit initial

---
+++ src/main/java/com/isaac/api/hortifruti/Api_hortifruti/ApiHortifrutiApplication.java
@@ -0,0 +1,13 @@
+package com.isaac.api.hortifruti.Api_hortifruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHortifrutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHortifrutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isaac/api/hortifruti/Api_hortifruti/ApiHortifrutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaac.api.hortifruti.Api_hortifruti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHortifrutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
